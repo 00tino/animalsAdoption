ad4f7b36531054966d5e6249b14ede70e27c3de5
00tino <fonteviva.valen.tino00@gmail.com>
2023-02-07 16:06:29 -0300

    responsive until 445px

--- assets/js/fixed.js
+++ assets/js/fixed.js
@@ -52,3 +52,5 @@ var bubblyButtons = document.getElementsByClassName("bubbly-button");
 for (var i = 0; i < bubblyButtons.length; i++) {
     bubblyButtons[i].addEventListener('click', animateButton, false);
 }
+
+// hide navbar
